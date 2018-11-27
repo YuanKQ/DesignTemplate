7c5cb24f9636f003c8106dd64f28d8557ffad76c
yuan <kq_yuan@outlook.com>
2018-11-27 20:20:34 +0800

    享元模式

--- src/Chap26Flyweight.java
+++ src/Chap26Flyweight.java
@@ -9,4 +9,48 @@
  * 现采用享元模式完成上述场景。
  */
 public class Chap26Flyweight {
+    public static void main(String[] args) {
+        Restuarant restuarant = new Restuarant();
+        System.out.println("table 1:");
+        restuarant.getOrder("regular");
+
+        System.out.println("table 2:");
+        restuarant.getOrder("chicken soup noodle");
+
+        System.out.println("table 1:");
+        restuarant.getOrder("fried potato and eggplant, mint beef and rice");
+    }
+}
+interface FoodSet {
+    void server(String name);
+}
+
+class RegularFoodSet implements FoodSet{
+    @Override
+    public void server(String name) {
+        System.out.println("serve: chicken mushroom stew, pickles and bamboo tube rice");
+    }
+}
+
+class FlexibleFoodSet implements FoodSet{
+    @Override
+    public void server(String name) {
+        String[] dishes = name.split(",");
+        System.out.print("serve: ");
+        for (String dish: dishes) {
+            System.out.print(dish + ",");
+        }
+        System.out.println();
+    }
+}
+
+class Restuarant {
+    RegularFoodSet regularFoodSet = new RegularFoodSet();
+    public void getOrder(String order) {
+        if (order != "regular") {
+            new FlexibleFoodSet().server(order);
+        } else {
+            regularFoodSet.server(order);
+        }
+    }
 }
